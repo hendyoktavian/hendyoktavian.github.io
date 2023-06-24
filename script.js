858d4f982979ffc6d865381de19d6fc2aa7258db
hendyoktavian <hendyoktavians@gmail.com>
2023-06-24 14:49:37 +0700

    ubah projects

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 //about
 
-$(window).on("load", function() {
+$(window).on("load", function () {
   $(".pKiri").addClass("pMuncul");
   $(".pKanan").addClass("pMuncul");
 });
-
-
 
 //paralax
 
@@ -13,15 +11,48 @@ $(window).scroll(function () {
   var wScroll = $(this).scrollTop();
 
   $(".jumbotron img").css({
-    transform: "translate(0px, +" + wScroll/5.7 + "%)"
+    transform: "translate(0px, +" + wScroll / 5.7 + "%)",
   });
 
   $(".jumbotron h1").css({
-    transform: "translate(0px, +" + wScroll/4.2 + "%)"
+    transform: "translate(0px, +" + wScroll / 4.2 + "%)",
   });
 
   $(".jumbotron p").css({
-    transform: "translate(0px, +" + wScroll/5.5 + "%)"
+    transform: "translate(0px, +" + wScroll / 5.5 + "%)",
   });
-
 });
+
+//projects
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("projectdot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" .projectdot:active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " .projectdot:active";
+}
